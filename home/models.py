88e177dc4f5caa30cc05099f1df26640f5e721b1
FirmoDaniel <firmodaniel@gmail.com>
2021-08-31 12:54:23 +0000

    Completed: Migrations and loaded data. UPDATED: home and product models.py and admin.py

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Testimonial(models.Model):
+
+    username = models.CharField(max_length=254)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.username
--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Product, Category
 
 # Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'category',
+        'name',
+        'description',
+        'price',
+        'rating',
+        'home',
+        'outdoors',
+        'day',
+        'night',
+        'image',
+    )
+
+    ordering = ('category',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
