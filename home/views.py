8b397744cf471cffb69d7ce07a072343b49584b9
FirmoDaniel <firmodaniel@gmail.com>
2021-08-31 13:43:22 +0000

    ADDED: products view and html. UPDATED: home app for testimonials and made migrations.

--- home/views.py
+++ home/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from products.models import Product
+from .models import Testimonial
 
 # Create your views here.
 
 
 def index(request):
-    """ Returns home page """
-    return render(request, 'home/index.html')
+    """
+    Returns Index Page which displays three highest arted products in cards
+     and customer testimonials on a list.
+    """
+    products = Product.objects.all()
+    testimonials = Testimonial.objects.all()
+
+    context = {
+        'products': products,
+        'testimonials': testimonials
+    }
+
+    return render(request, 'home/index.html', context)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+
+def all_products(request):
+    """ Returns Prodcts page and includes sorting """
+
+    products = Product.objects.all()  # returns all products
+
+    context = {
+        'products': products,  # products variable sent to template via context
+    }
+
+    return render(request, 'products/products.html', context)
