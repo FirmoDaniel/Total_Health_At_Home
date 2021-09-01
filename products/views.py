1346e84fca4f1451907800c043411e43c2f592ef
FirmoDaniel <firmodaniel@gmail.com>
2021-09-01 13:14:27 +0000

    ADDED: no_image to media. UPDATED: products and products details with filtering by category name.

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from django.contrib import messages
+from .models import Product, Category
 
 # Create your views here.
 
@@ -8,9 +9,17 @@ def all_products(request):
     """ Returns Prodcts page and includes sorting """
 
     products = Product.objects.all()  # returns all products
+    categories = None
+
+    if request.GET:  # sorting by categories
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')  # splits query into a list at the comma
+            products = products.filter(category__name__in=categories)  # filter products by those with matching category name
+            categories = Category.objects.filter(name__in=categories)  # filter categories down to ones existing in the url to show what categorys are veing viewed by friendly name
 
     context = {
         'products': products,  # products variable sent to template via context
+        'current_categories': categories
     }
 
     return render(request, 'products/products.html', context)
