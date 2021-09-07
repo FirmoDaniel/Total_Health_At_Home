825143d7db9595a1905fe00898d3ad7bd518d16b
FirmoDaniel <firmodaniel@gmail.com>
2021-09-07 11:47:35 +0000

    UPDATED: home app (model, fitures, view) to show only testimonials with approved set to True.

--- home/models.py
+++ home/models.py
@@ -7,6 +7,7 @@ class Testimonial(models.Model):
 
     username = models.CharField(max_length=254)
     description = models.TextField()
+    approved = models.BooleanField(default=False, null=False, blank=False)
 
     def __str__(self):
         return self.username
