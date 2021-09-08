e0f27cfa79a4e138e3fd584a82e67bbc1c342a74
FirmoDaniel <firmodaniel@gmail.com>
2021-09-08 12:59:16 +0000

    ADDED: checkout/stripe_elements.js. UPDATED: checkout.html, checkout views with stripe public key.

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51J5UpcHzjxwzuJvea84MutDVjFAl1t33Awphr56sAlUh7QpsnHpqH7vZogzBRcbRmiQxvjiEugDxS7lvcAx3P2Ng00UESCz2gz',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
