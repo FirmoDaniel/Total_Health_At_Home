282550648f7691aa5723cc1cbfea921f69ca9de4
FirmoDaniel <firmodaniel@gmail.com>
2021-09-08 11:56:03 +0000

    ADDED: checkout/urls.py,checkout/views.py, checkout.html, checkout/css. INSTALLED: crispy forms.

--- Total_Health_At_Home/settings.py
+++ Total_Health_At_Home/settings.py
@@ -47,6 +47,9 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+    #  OTHER
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -60,6 +63,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'Total_Health_At_Home.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -77,6 +82,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+            'builtins':[
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
