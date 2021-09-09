4450340795f549a1cf3b9941177a026d9095272f
FirmoDaniel <firmodaniel@gmail.com>
2021-09-09 13:04:42 +0000

    UPDATED: checkout and bag html with style and display changes.

--- checkout/forms.py
+++ checkout/forms.py
@@ -19,7 +19,7 @@ class OrderForm(forms.ModelForm):
             'full_name': 'Full Name',
             'email': 'Email Address',
             'phone_number': 'Phone Number',
-            'diet': 'Vegan/Vegitarian/Omnivore',
+            'diet': 'Specify Diet (Vegan//Omnivore etc)',
             'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
