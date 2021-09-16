6002207e86d12843382bdbda69d4d089d1539294
FirmoDaniel <firmodaniel@gmail.com>
2021-09-16 13:14:47 +0000

    UPDATED: profiles form, view, static/js, html to render form on profile page.

--- profiles/models.py
+++ profiles/models.py
@@ -16,7 +16,7 @@ class UserProfile(models.Model):
     default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
     default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
     default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
-    default_country = CountryField(blank_label='Country *', null=True, blank=True)
+    default_country = CountryField(blank_label='Country', null=True, blank=True)
 
     def __str__(self):
         return self.user.username
--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
@@ -8,9 +9,15 @@ def profile(request):
 
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    # populate form with current user's information
+    form = UserProfileForm(instance=profile)
+
+    orders = profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders,
     }
 
     return render(request, template, context)
