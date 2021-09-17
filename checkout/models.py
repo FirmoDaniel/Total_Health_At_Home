561dd369dda76d9d9bf732f3bfb482798562ef0c
FirmoDaniel <firmodaniel@gmail.com>
2021-09-17 12:41:13 +0000

    UPDATED: checkout.models with 'shortdate' to display D/M/Y on profile.html.

--- checkout/models.py
+++ checkout/models.py
@@ -24,12 +24,14 @@ class Order(models.Model):
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
-    date = models.DateField(default=datetime)
-    #  date = models.DateTimeField(auto_now_add=True)
+    date = models.DateTimeField(auto_now_add=True)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     original_bag = models.TextField(null=False, blank=False, default='')
     stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
+
+    def shortdate(self):
+        return self.date.strftime('%B %d %Y')
 
     def _generate_order_number(self):
         """
