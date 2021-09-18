5b04c0def25435e27835bea79bbb439ce20e9c36
FirmoDaniel <firmodaniel@gmail.com>
2021-09-18 15:31:54 +0000

    UPDATED: wh_handler to handle profiles. checkout_success to display first accordian when from profile.

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from .models import Order, OrderLineItem
 from products.models import Product
 
+from profiles.models import UserProfile
+
 import json
 import time
 
@@ -39,6 +41,19 @@ class StripeWH_Handler:
             if value == "":
                 shipping_details.address[field] = None
 
+        # Update profile information if save_info was checked
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.default_phone_number = shipping_details.phone
+                profile.default_country = shipping_details.address.country
+                profile.default_town_or_city = shipping_details.address.city
+                profile.default_street_address1 = shipping_details.address.line1
+                profile.default_street_address2 = shipping_details.address.line2
+                profile.save()
+
         order_exists = False
         attempt = 1
         while attempt <= 5:
@@ -69,6 +84,7 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                         full_name=shipping_details.name,
+                        user_profile=profile,
                         email=billing_details.email,
                         phone_number=shipping_details.phone,
                         country=shipping_details.address.country,
@@ -78,13 +94,13 @@ class StripeWH_Handler:
                         original_bag=bag,
                         stripe_pid=pid,
                     )
-                for item_id, quantity in json.loads(bag).items():
-                        product = Product.objects.get(id=item_id)
-                        order_line_item = OrderLineItem(
-                            order=order,
-                            product=product,
-                        )
-                        order_line_item.save()
+                for item_id, __ in json.loads(bag).items():
+                    product = Product.objects.get(id=item_id)
+                    order_line_item = OrderLineItem(
+                        order=order,
+                        product=product,
+                    )
+                    order_line_item.save()
             except Exception as e:
                 if order:
                     order.delete()
