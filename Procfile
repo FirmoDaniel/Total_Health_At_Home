69aa72b7ed42cc9c02f1d46e12e97f947b047290
FirmoDaniel <firmodaniel@gmail.com>
2021-10-01 12:06:26 +0000

    DEPLOY: part 2 complete. Note: attempting first push to heroku now.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Total_Health_At_Home.wsgi:application
--- Total_Health_At_Home/settings.py
+++ Total_Health_At_Home/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-=1^c$rz#ew^7=6i+)=k3^$q%d52u9p^=9=@5-)xz=nzg+o!)*3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['total-health-at-home.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -118,12 +118,19 @@ WSGI_APPLICATION = 'Total_Health_At_Home.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# this is differemt in ci example
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
