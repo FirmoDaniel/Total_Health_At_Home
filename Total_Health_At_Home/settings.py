48cb3f69ced0ec327e1ba1c3246cd1f22ff9e1af
FirmoDaniel <firmodaniel@gmail.com>
2021-10-01 15:40:54 +0000

    UPDATE: migrations run for changes to date fields in review and testimonial models.

--- Total_Health_At_Home/settings.py
+++ Total_Health_At_Home/settings.py
@@ -218,7 +218,6 @@ else:
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
