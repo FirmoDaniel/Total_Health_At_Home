2aa297a498c71fb7ac256164fb500efc2b9c6d3e
FirmoDaniel <firmodaniel@gmail.com>
2021-10-01 15:57:10 +0000

    ERROR: recieved an internal error 500 when registering a new user via heroku. Django logs the user request and awaits verification but email isn't sent.

--- Total_Health_At_Home/settings.py
+++ Total_Health_At_Home/settings.py
@@ -93,6 +93,9 @@ TEMPLATES = [
     },
 ]
 
+# added this from ci source code
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
